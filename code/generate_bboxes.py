f35cad5b5c25a1981e6f63ef975b1d6b9556d669
Chris Seals <seals_christopher@bah.com>
2019-09-05 11:12:23 -0500

    create outline

---
+++ code/generate_bboxes.py
@@ -0,0 +1,17 @@
+# Generate bounding boxes around the sea lions in the dotted images
+# These boxes are turned into image files and saved off in a folder structure
+
+# import os.path
+# if os.path.exists('../results/bbox_chips/adult_males'):
+#     print(True)
+
+# Identify filenames
+# Identify location of dotted images
+# Identify location of unmarked images
+
+# Extract dot locations
+# Create locations of sea lions
+# Iterate through the locations, extract bounding box by type of sea lion
+# 1. check for directory existence
+# 2. if not existing, create it
+# 3. save off chip for each bbox, and write chip path
